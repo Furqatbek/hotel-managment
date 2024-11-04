20ac16ced3c160ad6ec241de3a756fc705962e1c
Furqat <f.saydamadov@abflow.ru>
2024-11-04 19:30:45 +0500

    start of development

---
+++ src/test/java/uz/mehmonxona/managment/Mehmonxona/MehmonxonaApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.mehmonxona.managment.Mehmonxona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MehmonxonaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
